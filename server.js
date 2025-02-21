ce5f3dc2099467dac478cde91974fbed91af068b
yashin18 <Yashinh2000@yahoo.com>
2025-02-21 23:08:08 +0330

    Update server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ wss.on("connection", (ws) => {
 function startTimer(timerID) {
     timers[timerID].interval = setInterval(() => {
         if (timers[timerID].time > 0) {
-            timers[timerID].time-= 0.1;
+            timers[timerID].time = Math.max(0, Math.round((timers[timerID].time - 0.1) * 100) / 100);
             broadcast(timerID);
         } else {
             clearInterval(timers[timerID].interval);
